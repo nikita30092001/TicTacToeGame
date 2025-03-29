b4d09956e6e7bf9fd106da6607d489ebb68d929f
Nikita Sergeenko <nikitasergeenko2001@mail.ru>
2025-03-29 18:28:14 +0300

    Update NetworkManagerUI.cs

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -1,16 +1,27 @@
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Button _startHostButton;
+    [SerializeField] private Button _startClientButton;
+
+    private void Awake()
     {
-        
+        _startHostButton.onClick.AddListener(() => 
+        { 
+            NetworkManager.Singleton.StartHost();
+            Hide();
+        });
+        _startClientButton.onClick.AddListener(() => 
+        { NetworkManager.Singleton.StartClient();
+            Hide();
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Hide()
     {
-        
+        gameObject.SetActive(false);
     }
 }
